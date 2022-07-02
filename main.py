0e3985f5a3e523c733fa7b1c6b59d1412ab2d076
UndrscoreEX <ethan.heikell.official@gmail.com>
2022-07-02 10:50:47 +0900

    final commit before production

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, flash, request, url_for
 import pandas as pd
 from flask_bootstrap import Bootstrap
-from decouple import config
+# from decouple import config
 import os
 # from openpyxl import load_workbook
 
